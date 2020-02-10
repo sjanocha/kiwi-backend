777c91276a4bb7464b229c60a31b4fa51ab53e2c
sjanocha <45245645+sjanocha@users.noreply.github.com>
2020-02-10 04:17:45 +0100

    2

--- src/main/java/pl/studia/android/skyscanner/backend/SkyScannerAppBackendApplication.java
+++ src/main/java/pl/studia/android/skyscanner/backend/SkyScannerAppBackendApplication.java
@@ -2,16 +2,13 @@ package pl.studia.android.skyscanner.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-
-import pl.studia.android.skyscanner.backend.controller.RestApiController;
 
 @SpringBootApplication
 public class SkyScannerAppBackendApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 
-		SpringApplication.run(SkyScannerAppBackendApplication.class, args);
-	}
+        SpringApplication.run(SkyScannerAppBackendApplication.class, args);
+    }
 
 }
--- src/main/java/pl/studia/android/skyscanner/backend/model/ModelRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.studia.android.skyscanner.backend.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ModelRepository extends JpaRepository<TestModelClass, Long> {}
